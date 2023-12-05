78fcd565035efb8c1fec91117bafed0c29a5c6b7
RobinFlaks <robin_flaks@hotmail.com>
2023-12-05 04:09:41 +0100

    Initial commit

---
+++ src/main/java/com/robin/pg3402/Pg3402Application.java
@@ -0,0 +1,13 @@
+package com.robin.pg3402;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pg3402Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pg3402Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/robin/pg3402/Pg3402ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robin.pg3402;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pg3402ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
