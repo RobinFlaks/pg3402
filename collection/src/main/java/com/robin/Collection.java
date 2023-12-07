221ba9183917348286ba1a9d96a3df04cb97e54a
RobinFlaks <robin_flaks@hotmail.com>
2023-12-07 12:23:41 +0100

    started creating microservices structure

---
+++ collection/src/main/java/com/robin/Collection.java
@@ -0,0 +1,11 @@
+package com.robin;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Collection {
+
+    private String cards;
+}
---
+++ shared_objects/src/main/java/com/robin/Card.java
@@ -0,0 +1,7 @@
+package com.robin;
+
+public class Card {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
